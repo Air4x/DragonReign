ecda723641eae88b09cd2d46cc371d23ee5ac5d4
Mario Calcagno <mario.calcagno@protonmail.com>
2025-12-23 21:53:14 +0100

    Added packages

--- home.scm
+++ home.scm
@@ -8,30 +8,102 @@
              (gnu packages perl6)	     
              (gnu packages lisp)
 	     (gnu packages mpd)
+	     (gnu packages emacs)
+	     (gnu packages emacs-xyz)
+	     (gnu packages fonts)
+	     (gnu packages wm)
+	     (gnu packages syncthing)
              (guix gexp))
 
 
-(home-environment
-  (packages (list htop
-		  perl
-		  sbcl
-		  rakudo
-		  mpd
-		  mpd-mpc
-		  coreutils
-		  binutils))
-  (services
-   (append (list
-            (service home-zsh-service-type
-                     (home-zsh-configuration
-                      (xdg-flavor? #f)
-		      (environment-variables
-		       '(("PROVA" . "Ciao, Guix")))))
+(define base-packages
+  '(coreutils
+    binutils
+    util-linux
+    htop
+    acpi))
 
-            (simple-service 'test-config
-                            home-xdg-configuration-files-service-type
-                            (list `("test.conf"
-                                    ,(plain-file "tmp-file.txt"
-                                                 "the content of
-                                          ~/.config/test.conf")))))
-           %base-home-services)))
+(define emacs-stuff
+  '(emacs-pgtk
+    emacs-telega
+    emacs-raku-mode
+    emacs-slime
+    emacs-all-the-icons
+    emacs-ligature
+    emacs-mixed-pitch
+    emacs-consult
+    emacs-vertico
+    emacs-marginalia
+    emacs-orderless
+    emacs-embark
+    emacs-org
+    emacs-jinx
+    emacs-olivetti
+    emacs-org-roam
+    emacs-org-contacts
+    emacs-gnuplot
+    emacs-org-bullets
+    emacs-org-appear
+    emacs-htmlize
+    emacs-pdf-tools
+    emacs-org-tree-slide
+    emacs-eat
+    emacs-yasnippet
+    emacs-yasnippet-snippets
+    emacs-paredit
+    emacs-rainbow-delimeters
+    emacs-geiser
+    emacs-geiser-guile
+    emacs-corfu
+    emacs-cape
+    emacs-magit
+    emacs-auctex
+    emacs-elpher))
+
+(define fonts
+  '(font-fira-code))
+
+(define scripting
+  '(rakudo
+    perl
+    zef
+    cpan))
+
+(define programming
+  '(sbcl
+    guile))
+
+(define music
+  '(mpd
+    mpd-mpc
+    ncmpcpp))
+
+(define sway-stuff
+  '(sway
+    swayidle
+    swaylock
+    swaybg
+    waybar))
+
+
+(define desktop
+  (append sway-stuff '(tofi
+		       cliphist
+		       wl-clipboard
+		       gammastep
+		       grim
+		       slurp
+		       dunst
+		       syncthing)))
+
+(home-environment
+ (packages (append base-packages
+		   emacs-stuff
+		   music
+		   scripting
+		   programming
+		   desktop))
+ (services
+  (append (list
+           (service home-zsh-service-type))
+          %base-home-services)))
